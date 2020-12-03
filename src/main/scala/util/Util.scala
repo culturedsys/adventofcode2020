08166de7334cba9ca589e2e85d74fcde988fe14f
Tim Fisken <contact@timfisken.org>
2020-12-03 19:26:47 +0000

    Generify report function

--- src/main/scala/util/Util.scala
+++ src/main/scala/util/Util.scala
@@ -2,15 +2,17 @@ package util
 
 import cats.effect.IO
 import cats.effect.ExitCode
+import cats.Show
+import cats.syntax._
 
 object Util {    
-    def report: Option[Int] => IO[ExitCode] = {
+    def report[T: Show](result: Option[T]): IO[ExitCode] = result match {
         case None => IO {
             println("Error")
             ExitCode.Error
         }
-        case Some(result) => IO { 
-            println(result)
+        case Some(result) => IO {
+            println(implicitly[Show[T]].show(result))
             ExitCode.Success 
         }
     }
