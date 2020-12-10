66a7f6354da565201d3ab64599a2cfba70c18a4b
Tim Fisken <contact@timfisken.org>
2020-12-10 22:32:42 +0000

    Add execute function to Util
    Adds a function, execute, which evaluates its (lazy) argument, and then
    prints the time taken in evaluation along with the result, returning an
    exitcode.

--- src/main/scala/util/Util.scala
+++ src/main/scala/util/Util.scala
@@ -4,6 +4,9 @@ import cats.effect.IO
 import cats.effect.ExitCode
 import cats.Show
 import cats.syntax._
+import scala.io.Source
+import cats.effect.Clock
+import java.util.concurrent.TimeUnit
 
 object Util {    
     def report[T: Show](result: Either[Any, T]): IO[ExitCode] = result match {
@@ -18,6 +21,14 @@ object Util {
     }
 
     def report[T: Show](result: Option[T]): IO[ExitCode] = report(result.toRight("Error"))
+
+    def execute[A: Show](f: => Option[A])(implicit clock: Clock[IO]): IO[ExitCode] = for {
+        start <- clock.monotonic(TimeUnit.MILLISECONDS)
+        result = f
+        end <- clock.monotonic(TimeUnit.MILLISECONDS)
+        _ <- IO { println(s"Time: ${end - start}") }
+        exitcode <- report(result)
+    } yield exitcode
 
     def splitIf[T](pred: T => Boolean, source: Iterator[T]): Seq[Seq[T]] = {
         def go(result: Seq[Seq[T]], partial: Seq[T], remaining: Seq[T]): Seq[Seq[T]] = {
@@ -35,5 +46,9 @@ object Util {
         }
 
         go(Seq[Seq[T]](), Seq[T](), source.toSeq)
+    }
+
+    def readInput(path: String) = IO {
+        Source.fromResource(path).getLines()
     }
 }
