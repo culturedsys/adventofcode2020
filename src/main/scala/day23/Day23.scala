a1764a9afdb7ec6156c87f578aec5f0a8e03713c
Tim Fisken <contact@timfisken.org>
2020-12-24 15:16:09 +0000

    Day 23 part 2
    Rewrites Day 23 to use a mutable, circular linked list, to speed up
    modifications. Also adds a map from label to cup, to avoid a linear scan
    through all cups.

--- src/main/scala/day23/Day23.scala
+++ src/main/scala/day23/Day23.scala
@@ -3,32 +3,104 @@ package day23
 import cats.effect.IOApp
 import cats.effect.{ExitCode, IO}
 import util.Util
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable
+
+class CupCircle(var head: CupCircle.Cup, val labelToCup: Map[Int, CupCircle.Cup]) {
+    def toSeq: Seq[Int] = {
+        val result = ListBuffer[Int]()
+        var pointer = head
+        do {
+            result.addOne(pointer.label)
+            pointer = pointer.next
+        } while (pointer != head)
+
+        result.toSeq
+    }
+
+    def rotate: Unit = {
+        head = head.next
+    }
+
+    def find(label: Int): CupCircle.Cup = 
+        labelToCup(label)
+
+    def insertAfter(label: Int, first: CupCircle.Cup, last: CupCircle.Cup): Unit = {
+        val target = find(label)
+        last.next = target.next
+        target.next = first
+    }
+
+    def remove(n: Int): (CupCircle.Cup, CupCircle.Cup) = {
+        var target = head.next
+        val result = target
+
+        (0 until n - 1).foreach{ _ => 
+            target = target.next
+        }
+
+        head.next = target.next
+        (result, target)
+    }
+}
+
+object CupCircle {
+    class Cup(var label: Int = 0, var next: Cup = null) {
+        def isIn(value: Int, n: Int): Boolean = {
+            var target = this
+            var i = 0
+            while (i < n) {
+                if (target.label == value)
+                    return true
+                target = target.next
+                i += 1
+            }
+
+            return false
+                
+        }
+    }
+
+    def apply(values: Int*): CupCircle = {
+        apply(values.iterator)
+    }
+
+    def apply(values: Iterator[Int]): CupCircle = {
+        val (head, last, labelToCup) = fromIterator(values)
+        last.next = head
+        new CupCircle(head, labelToCup)
+    }
+
+    def fromIterator(values: Iterator[Int]): (Cup, Cup, Map[Int, Cup]) = {
+        val first = new Cup(values.next(), new Cup())
+        var last = first
+        var  labelToCup = mutable.Map[Int, Cup](first.label -> first)
+
+        while (values.hasNext) {
+            last = last.next
+            last.label = values.next()
+            labelToCup(last.label) = last
+            last.next = new Cup()
+        }
+
+        (first, last, labelToCup.toMap)
+    }
+}
 
 object Day23 {
-    def rotate(v: Vector[Int]): Vector[Int] = v match {
-        case head +: tail => tail :+ head
-        case _ => Vector()
+    def move(v: CupCircle, min: Int, max: Int): Unit = {
+        val (picked, last) = v.remove(3)
+        var target = v.head.label
+        do {
+            target = if (target - 1 < min) max else target - 1            
+        } while (picked.isIn(target, 3))     
+
+        v.insertAfter(target, picked, last)
+        v.rotate
     }
 
-    def insertAfter(v: Vector[Int], target: Int, addition: Seq[Int]): Vector[Int] = {
-        val (before, after) = v.span(_ != target)
-        (before :+ after.head) ++ addition ++ after.tail 
-    }
-
-    def remove(v: Vector[Int], n: Int): (Seq[Int], Vector[Int]) =
-        (v.tail.take(n), v.head +: v.tail.drop(n))
-
-    def move(v: Vector[Int]): Vector[Int] = {
-        val (picked, remainder) = remove(v, 3)
-        val min = v.min
-        val max = v.max
-        def decrementWrap(candidate: Int) = if (candidate - 1 < min) max else candidate - 1
-        val target = Stream.iterate(v.head)(decrementWrap)
-            .tail.dropWhile(picked.contains(_)).head
-        rotate(insertAfter(remainder, target, picked))
-    }
-
-    def formatResult(v: Vector[Int]): String = {
+    def formatResult(v: Seq[Int]): String = {
         val (before, after) = v.span(_ != 1)
         (after.tail ++ before).mkString
     }
@@ -37,8 +109,32 @@ object Day23 {
 object Part1 extends IOApp {
     import Day23._
 
-    override def run(args: List[String]): IO[ExitCode] = Util.execute {
-        val result = Stream.iterate(Vector(1, 2, 3, 4, 8, 7, 5, 9, 6))(move).drop(100).head
-        Some(formatResult(result))
-    } 
+    override def run(args: List[String]): IO[ExitCode] = for {
+        buffer <- IO { CupCircle(1, 2, 3, 4, 8, 7, 5, 9, 6) }        
+        exitcode <- Util.execute {
+            (0 until 100).foreach { _ => 
+                move(buffer, 1, 9)
+            }
+            Some(formatResult(buffer.toSeq))
+        }
+    } yield exitcode 
+}
+
+object Part2 extends IOApp {
+    import Day23._
+
+    override def run(args: List[String]): IO[ExitCode] = {
+        val buffer = { 
+            val init = Stream(1, 2, 3, 4, 8, 7, 5, 9, 6) ++ Stream.from(10 to 1_000_000)
+            CupCircle(init.iterator) 
+        }
+        
+        Util.execute {
+            (0 until 10_000_000).foreach { _ => 
+                move(buffer, 1, 1_000_000)
+            }
+            val target = buffer.find(1)
+            Some(target.next.label.toLong * target.next.next.label)
+        }
+    }
 }
--- src/test/scala/day23/Day23Spec.scala
+++ src/test/scala/day23/Day23Spec.scala
@@ -6,27 +6,55 @@ import org.scalatest.matchers.should.Matchers
 class Day23Spec extends AnyFlatSpec with Matchers {
     import Day23._
 
-    "rotate" should "rotate the sequence" in {
-        rotate(Vector(1, 2, 3, 4)) shouldBe Seq(2, 3, 4, 1)
+    "CircularBuffer#toSeq" should "produce the correct sequence" in {
+        CupCircle(1, 2, 3).toSeq shouldBe Seq(1, 2, 3)
     }
 
-    "insertAfter" should "insert after" in {
-        insertAfter(Vector(1, 2, 3), 2, Seq(5, 6)) shouldBe Vector(1, 2, 5, 6, 3)            
+    "CircularBuffer#rotate" should "rotate the sequence" in {
+        val buffer = CupCircle(1, 2, 3, 4)
+        buffer.rotate
+        buffer.toSeq shouldBe Seq(2, 3, 4, 1)
     }
 
-    "insertAfter" should "insert when target is end" in {
-        insertAfter(Vector(1, 2, 3), 3, Seq(5, 6)) shouldBe Vector(1, 2, 3, 5, 6)            
+    "CircularBuffer#insertAfter" should "insert after" in {
+        val buffer = CupCircle(1, 2, 3)
+        val (insStart, insEnd, _) = CupCircle.fromIterator(Seq(5, 6).iterator)
+
+        buffer.insertAfter(2, insStart, insEnd)
+        
+        buffer.toSeq shouldBe Seq(1, 2, 5, 6, 3)            
+    }
+
+    "CircularBuffer#insertAfter" should "insert when target is end" in {
+        val buffer = CupCircle(1, 2, 3)
+        val (insStart, insEnd, _) = CupCircle.fromIterator(Seq(5, 6).iterator)
+      
+        buffer.insertAfter(3, insStart, insEnd)
+        
+        buffer.toSeq shouldBe Seq(1, 2, 3, 5, 6)            
     }
 
     "remove" should "remove n after head" in {
-        remove(Vector(1, 2, 3, 4, 5), 3) shouldBe (Seq(2, 3, 4), Vector(1, 5))
+        val buffer = CupCircle(1, 2, 3, 4, 5)
+        
+        buffer.remove(3)
+        
+        buffer.toSeq shouldBe Seq(1, 5)
     }
 
     "move" should "complete move" in {
-        move(Vector(3, 8, 9, 1, 2, 5, 4, 6, 7)) shouldBe Vector(2, 8, 9, 1, 5, 4, 6, 7, 3)
+        val buffer = CupCircle(3, 8, 9, 1, 2, 5, 4, 6, 7)
+        
+        move(buffer, 1, 9) 
+        
+        buffer.toSeq shouldBe Seq(2, 8, 9, 1, 5, 4, 6, 7, 3)
     }
 
     "move" should "complete move requiring wrap" in {
-        move(Vector(5, 4, 6, 7, 8, 9, 1, 3, 2)) shouldBe Vector(8, 9, 1, 3, 4, 6, 7, 2, 5)
+        val buffer = CupCircle(5, 4, 6, 7, 8, 9, 1, 3, 2)
+
+        move(buffer, 1, 9) 
+        
+        buffer.toSeq shouldBe Vector(8, 9, 1, 3, 4, 6, 7, 2, 5)
     }
 }
