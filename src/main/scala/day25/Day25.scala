44fabc8bc7b66168b9186cb0a0abc83a70e31d09
Tim Fisken <contact@timfisken.org>
2020-12-27 14:40:25 +0000

    Speed up Day 25
    Uses the baby step, giant-step algorithm to find the loop size in
    O(sqrt(modulus)) rather than O(modulus) time.

--- src/main/scala/day25/Day25.scala
+++ src/main/scala/day25/Day25.scala
@@ -5,11 +5,35 @@ import cats.effect.{ExitCode, IO}
 import util.Util
 
 object Day25 {
-    def findLoopSize(subjectNumber: Int, prime: Int, publicKey: Int): Int = 
-        Iterator.iterate(1L)(value => (value.toLong * subjectNumber) % prime)
-            .zipWithIndex
-            .dropWhile(_._1 != publicKey)
-            .buffered.head._2
+    /**
+     * Find x where alpha ^ x = beta (mod modulus)
+     * 
+     * Works by finding i, m, j such that alpha ^ (i * m + j) = beta, which is equivalent to
+     * alpha ^ j = beta * (alpha ^ -m) ^ i
+     * 
+    */
+    def discreteLog(alpha: Int, beta: Int, modulus: Int): Int = {
+        // Fix m as sqrt(modulus)
+        val m = math.ceil(math.sqrt(modulus)).toInt
+
+        // Calculate values of alpha ^ j where 0 <= j < m
+        val alphaPrime = BigInt(alpha)
+        val alphaToJMap = (0 until m).map(j => (alphaPrime.modPow(j, modulus).toLong, j)).toMap
+
+        val alphaToMinusM = alphaPrime.modPow(-m, modulus)
+
+        // Find value of i, j where beta * (alpha ^ -m) ^ i = alpha ^ j
+        val (i, j) = (0 until m).map { i =>
+            alphaToJMap.get(((beta * alphaToMinusM.modPow(i, modulus)) % modulus).toLong)
+                .map(j => (i, j))
+        }.flatten.head
+
+        // Calculate x given m, i, j
+        i * m + j
+    }
+
+    def findLoopSize(subjectNumber: Int, modulus: Int, publicKey: Int): Int = 
+        discreteLog(subjectNumber, publicKey, modulus)
 
     def deriveKey(subjectNumber: Int, prime: Int, loopSize: Int): Long = 
         BigInt(subjectNumber).modPow(loopSize, prime).toLong
@@ -27,5 +51,4 @@ object Part1 extends IOApp {
             Some(encryptionKey)
         }
     }
-
 }
